51532de8e8475c8cb6e275311745ab4b181f4418
Florian Buetler <flbuetle@ethz.ch>
2020-11-24 21:56:16 +0100

    print output of similar_words to a file in json

--- scripts/similar_words.py
+++ scripts/similar_words.py
@@ -39,26 +39,30 @@ resp = requests.get(
 
 words = list(map(lambda x: x.lower(), resp.text.split("\n")[:-1]))
 
-sim = dict()
+sim_insert = dict()
+sim_remove = dict()
+sim_change = dict()
 l = len(words)
 for i, a in enumerate(words):
-    if len(a) == 1:
+    if len(a) <= 4 or "." in a:
         continue
-    matches = list()
+    insert = list()
+    remove = list()
+    change = list()
     for b in words:
-        if a == b or len(b) == 1:
+        if a == b or len(b) == 1 or "." in b:
             continue
 
         # insert
         for letter in list(map(chr, range(ord("a"), ord("z") + 1))):
             for pos in range(len(a) + 1):
-                if a[:pos] + letter + a[pos:] == b:
-                    matches.append(b)
+                if a[:pos] + letter + a[pos:] == b and not b in insert:
+                    insert.append(b)
 
         # remove
         for pos in range(len(a)):
-            if a[:pos] + a[pos + 1 :] == b:
-                matches.append(b)
+            if a[:pos] + a[pos + 1 :] == b and not b in remove:
+                remove.append(b)
 
         # change
         if len(a) != len(b):
@@ -66,12 +70,27 @@ for i, a in enumerate(words):
         distance = 0
         for pos in range(len(a)):
             distance += 1 if a[pos] != b[pos] else 0
-        if distance == 1:
-            matches.append(b)
+        if distance == 1 and not b in change:
+            change.append(b)
 
-    if len(matches) != 0:
-        sim[a] = matches
+    if len(insert) != 0:
+        sim_insert[a] = insert
+    if len(remove) != 0:
+        sim_remove[a] = remove
+    if len(change) != 0:
+        sim_change[a] = change
     printProgressBar(i + 1, l, prefix="Progress:", suffix="Complete", length=50)
 
-for k, v in sorted(sim.items(), key=lambda elem: len(elem[1])):
-    print("{} - {}".format(k, v))
+with open("words.json", "w") as f:
+    f.write("{\n")
+    for name, sim in [
+        ('"insert"', sim_insert),
+        ('"remove"', sim_remove),
+        ('"change"', sim_change),
+    ]:
+        f.write(name + ": {\n")
+        for k, v in sorted(sim.items(), key=lambda elem: len(elem[1])):
+            if len(v) >= 3:
+                f.write('"{}": {},\n'.format(k, v))
+        f.write("},\n")
+    f.write("}\n")
