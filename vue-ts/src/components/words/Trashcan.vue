6a3500edd72026d0c817af6e1ea911aedee915ea
Florian Buetler <flbuetle@ethz.ch>
2020-11-26 18:55:34 +0100

    add trashcan component

---
+++ vue-ts/src/components/words/Trashcan.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="trashcan"
+    @dragover.prevent
+    @dragenter.prevent
+    @drop.stop.prevent="$emit('trashed-element', $event)"
+  >
+    <img :src="require('@/assets/words/trashcan.png')" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component<Trashcan>({})
+export default class Trashcan extends Vue {}
+</script>
+
+<style scoped>
+.trashcan {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 3px dashed black;
+}
+.trashcan > img {
+  width: 100%;
+  max-width: 80px;
+  margin: 1em;
+}
+</style>
