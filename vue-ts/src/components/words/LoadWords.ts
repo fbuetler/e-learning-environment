c8a30e9fdfe61473594b05606128f99365d71067
Florian Buetler <flbuetle@ethz.ch>
2020-12-01 17:20:07 +0100

    fix no-explicit-any

--- vue-ts/src/components/words/LoadWords.ts
+++ vue-ts/src/components/words/LoadWords.ts
@@ -7,14 +7,14 @@ export type wordElement = {
   locked: boolean;
 };
 
-export function LoadWords(key: string): [any[], string[]] {
+export function LoadWords(key: string): [wordElement[], string[]] {
   const allWords = words;
   const keys = Object.keys(allWords[key]);
   const chosenWord = keys[Math.floor(Math.random() * keys.length)];
   const wordParts = chosenWord.split("");
 
   const similarWords = allWords[key][chosenWord];
-  const word = new Array<any>();
+  const word = new Array<wordElement>();
   for (let i = 0; i < wordParts.length; i++) {
     word.push({
       id: i,
