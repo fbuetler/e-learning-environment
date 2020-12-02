731e328b3ed9ca1b02411e18724366c0f059983f
Florian Buetler <flbuetle@ethz.ch>
2020-12-02 22:06:41 +0100

    implement mayas/from

---
+++ vue-ts/src/components/mayas/Addition.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component<Addition>({})
+export default class Addition extends Vue {}
+</script>
+
+<style scoped></style>
--- vue-ts/src/components/mayas/From.vue
+++ vue-ts/src/components/mayas/From.vue
@@ -1,1 +1,84 @@
+<template>
+  <div>
+    <div class="input-container" @click="addItem()">
+      <div>
+        <div class="nuts">
+          <div class="nut" v-for="index in items[nut]" :key="index">
+            <img :src="require('@/assets/mayas/nut.png')" />
+          </div>
+        </div>
+        <div class="sticks">
+          <div class="stick" v-for="index in items[stick]" :key="index">
+            <img :src="require('@/assets/mayas/stick.png')" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr />
+    <input v-model.number="number" type="number" />
+  </div>
+</template>
 
+<script lang="ts">
+import { Vue, Component, Mixins } from "vue-property-decorator";
+import GameMixin, { GameInterface } from "../Game";
+import { itemType } from "./NutsAndSticks.vue";
+
+@Component<From>({})
+export default class From extends Mixins(GameMixin) implements GameInterface {
+  private number: number = null;
+  private items: Array<itemType> = null;
+
+  private maxItems = 4;
+
+  isStarted(): boolean {
+    return this.items === null;
+  }
+
+  restartGame() {
+    this.items = new Array<number>(Object.keys(itemType).length / 2);
+    for (let i = 0; i < this.items.length; i++) {
+      this.items[i] = Math.ceil(Math.random() * this.maxItems);
+    }
+  }
+
+  isCorrect(): boolean {
+    return (
+      this.number === this.items[itemType.NUT] + this.items[itemType.STICK] * 5
+    );
+  }
+
+  get nut(): number {
+    return itemType.NUT;
+  }
+
+  get stick(): number {
+    return itemType.STICK;
+  }
+}
+</script>
+
+<style scoped>
+.input-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 5rem;
+  border: dashed 3px black;
+  border-radius: 15px;
+  background-color: lightsalmon;
+}
+.nuts {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+.sticks {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
