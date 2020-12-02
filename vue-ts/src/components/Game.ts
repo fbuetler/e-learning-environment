1934ecbe45418a29c061efc9982b8f2fa3810d55
Florian Buetler <flbuetle@ethz.ch>
2020-12-02 16:54:08 +0100

    introduce game mixin

---
+++ vue-ts/src/components/Game.ts
@@ -0,0 +1,38 @@
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import { EventBus, EventBusEvents } from "./EventBus";
+
+export interface GameInterface {
+  isStarted(): boolean;
+  restartGame(): void;
+  isCorrect(): boolean;
+}
+
+@Component
+export default class GameMixin extends Vue {
+  private notOverwrittenMsg =
+    "THIS SHOULD NOT BE EXECUTED! DID YOU FORGET TO OVERWRITE THIS FUNC?";
+  created() {
+    if (this.isStarted()) {
+      this.restartGame();
+    }
+    EventBus.$on(EventBusEvents.RestartGame, () => this.restartGame());
+    EventBus.$on(EventBusEvents.EvaluateGame, () => this.evaluateGame());
+  }
+
+  isStarted(): boolean {
+    throw Error(this.notOverwrittenMsg);
+  }
+
+  restartGame() {
+    throw Error(this.notOverwrittenMsg);
+  }
+
+  evaluateGame() {
+    this.$emit("evaluated-game", this.isCorrect());
+  }
+
+  isCorrect(): boolean {
+    throw Error(this.notOverwrittenMsg);
+  }
+}
