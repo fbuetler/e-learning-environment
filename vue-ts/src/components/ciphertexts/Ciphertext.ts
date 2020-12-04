124db90a6398d84104eda25b420ed89426caa0d2
Florian Buetler <flbuetle@ethz.ch>
2020-12-04 15:44:18 +0100

    add random nouns

--- vue-ts/src/components/ciphertexts/Ciphertext.ts
+++ vue-ts/src/components/ciphertexts/Ciphertext.ts
@@ -1,1 +1,10 @@
 // https://developer.mozilla.org/de/docs/Web/Guide/HTML/Canvas_Tutorial
+
+import data from "@/assets/ciphertexts/ciphertexts.json";
+
+export function LoadRandomElement(key: string): string {
+  const elems = data[key];
+  const elem = elems[Math.floor(Math.random() * elems.length)];
+  console.log(elem);
+  return elem;
+}
