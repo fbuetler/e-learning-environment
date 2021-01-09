d454fcc587a9e5acb7656d73f37d525f5b252a96
Florian Buetler <flbuetle@ethz.ch>
2021-01-09 01:49:35 +0100

    add unit tests for words/add

--- vue-ts/tests/unit/components/words/Add.spec.ts
+++ vue-ts/tests/unit/components/words/Add.spec.ts
@@ -1,17 +1,39 @@
 import { shallowMount } from "@vue/test-utils";
 import Add from "@/components/words/Add.vue";
+import { wordElement } from "@/components/words/Words";
 
+function prepareWord(word: string): wordElement[] {
+  return word.split("").map((el, i) => {
+    return { id: i, char: el, initialChar: el, locked: true };
+  });
+}
+
+function addToWord(
+  word: wordElement[],
+  char: string,
+  pos: number
+): wordElement[] {
+  word.splice(pos, 0, {
+    id: Math.max(...word.map((el) => el.id)) + 1,
+    char: char,
+    initialChar: "",
+    locked: false,
+  });
+  return word;
+}
+
+function copy(el) {
+  return JSON.parse(JSON.stringify(el));
+}
+
+const rawWord = "ADLER";
+const mockWord = prepareWord(rawWord);
+const mockSimilarWords = ["RADLER", "TADLER", "ADLERN", "ADLERS"];
 jest.mock("@/components/words/Words", () => ({
   LoadWords() {
     return [
-      [
-        { id: 0, char: "A", initialChar: "A", locked: true },
-        { id: 1, char: "D", initialChar: "D", locked: true },
-        { id: 2, char: "L", initialChar: "L", locked: true },
-        { id: 3, char: "E", initialChar: "E", locked: true },
-        { id: 4, char: "R", initialChar: "R", locked: true },
-      ],
-      ["RADLER", "TADLER", "ADLERN", "ADLERS"],
+      JSON.parse(JSON.stringify(mockWord)),
+      JSON.parse(JSON.stringify(mockSimilarWords)),
     ];
   },
 }));
@@ -31,6 +53,56 @@ describe("Add.vue", () => {
   });
 
   it("mock used correctly", () => {
-    expect(wrapper.vm["word"].map((el) => el.char).join("")).toBe("ADLER");
+    expect(wrapper.vm["word"].map((el) => el.char).join("")).toBe(rawWord);
   });
+
+  it("initially all chars are locked and nothing is added", () => {
+    expect(wrapper.vm["word"].every((el) => el.locked)).toBeTruthy();
+    expect(wrapper.vm["charAdded"]).toBeFalsy();
+  });
+
+  for (let i = 0; i <= mockWord.length; i++) {
+    it(`adding a char by click is possible at possition ${i}`, async () => {
+      await wrapper.setData({ selectedChar: "F" });
+      wrapper
+        .findAll("rect")
+        .at(i)
+        .trigger("click");
+      const newWord = addToWord(copy(mockWord), "F", i);
+      expect(wrapper.vm["word"]).toEqual(newWord);
+    });
+  }
+
+  it("no char selected", () => {
+    wrapper.vm.addChar(0);
+    expect(copy(wrapper.vm["word"])).toEqual(copy(mockWord));
+  });
+
+  it("add a char twice", async () => {
+    await wrapper.setData({ selectedChar: "F" });
+    wrapper.vm.addChar(0);
+    const word = copy(wrapper.vm["word"]);
+    await wrapper.setData({ selectedChar: "F" });
+    wrapper.vm.addChar(0);
+    expect(copy(wrapper.vm["word"])).toEqual(word);
+  });
+
+  it("correct word is evaluated as correct", async () => {
+    await wrapper.setData({ word: prepareWord(mockSimilarWords[0]) });
+    expect(wrapper.vm.isCorrect()).toBeTruthy();
+  });
+
+  it("incorrect word is evaluated as incorrect", async () => {
+    await wrapper.setData({ word: prepareWord("TESTTEST") });
+    expect(wrapper.vm.isCorrect()).toBeFalsy();
+  });
+
+  for (let i = 0; i <= mockWord.length; i++) {
+    it(`undo is handled correctly at position ${i}`, async () => {
+      await wrapper.setData({ word: addToWord(copy(mockWord), "F", i) });
+      wrapper.vm.undo();
+      expect(copy(wrapper.vm["word"])).toEqual(mockWord);
+      expect(wrapper.vm["charAdded"]).toBeFalsy();
+    });
+  }
 });
